61d904d7dc978c3f6112c21df8c17953a53f600d
Raz Aranyi <RazAranyi@gmail.com>
2024-05-04 11:29:05 +0300

    firt implement move business management to a common page

--- app/src/main/java/com/example/eassyappointmentfe/adapters/AppointmentsAdapter.java
+++ app/src/main/java/com/example/eassyappointmentfe/adapters/AppointmentsAdapter.java
@@ -43,15 +43,31 @@ public class AppointmentsAdapter extends RecyclerView.Adapter<AppointmentsAdapte
         holder.startTimeTextView.setBackgroundColor(ContextCompat.getColor(holder.startTimeTextView.getContext(), R.color.imageTextBackground));
         holder.endTimeTextView.setBackgroundColor(ContextCompat.getColor(holder.endTimeTextView.getContext(), R.color.imageTextBackground));
 
-        if (appointment.isAvailable()) {
-            holder.nameTextView.setVisibility(View.VISIBLE);
-            holder.cancelButton.setVisibility(View.VISIBLE);
-            holder.nameTextView.setBackgroundResource(R.color.imageTextBackground);
+        if (isCustomer) {
+            if (appointment.isAvailable()) {
+                holder.nameTextView.setVisibility(View.GONE);
+                holder.cancelOrBookButton.setVisibility(View.VISIBLE);
+                holder.cancelOrBookButton.setText("Book");
+                holder.nameTextView.setBackgroundResource(R.color.imageTextBackground);
 
-            holder.nameTextView.setText("Booking Name"); // get this from server or appointment object
-        } else {
-            holder.nameTextView.setVisibility(View.GONE);
-            holder.cancelButton.setVisibility(View.GONE);
+            } else {
+                holder.nameTextView.setVisibility(View.GONE);
+                holder.cancelOrBookButton.setEnabled(false);
+            }
+        }else {
+            if(appointment.isAvailable()) {
+                holder.cancelOrBookButton.setText("Cancel");
+                holder.cancelOrBookButton.setEnabled(false);
+                holder.nameTextView.setVisibility(View.VISIBLE);
+                holder.nameTextView.setText("Free");
+                holder.nameTextView.setBackgroundResource(R.color.imageTextBackground);
+            }else {
+                holder.nameTextView.setVisibility(View.VISIBLE);
+                holder.nameTextView.setText("Name"); //get from Server
+                holder.cancelOrBookButton.setText("Cancel");
+                holder.cancelOrBookButton.setEnabled(true);
+                holder.nameTextView.setBackgroundResource(R.color.imageTextBackground);
+            }
         }
     }
 
@@ -68,14 +84,14 @@ public class AppointmentsAdapter extends RecyclerView.Adapter<AppointmentsAdapte
 
     class AppointmentViewHolder extends RecyclerView.ViewHolder {
         private final TextView startTimeTextView, endTimeTextView, nameTextView;
-        private final Button cancelButton;
+        private final Button cancelOrBookButton;
 
         public AppointmentViewHolder(View itemView) {
             super(itemView);
             startTimeTextView = itemView.findViewById(R.id.start_time);
             endTimeTextView = itemView.findViewById(R.id.end_time);
             nameTextView = itemView.findViewById(R.id.name_text_view);
-            cancelButton = itemView.findViewById(R.id.cancel_button);
+            cancelOrBookButton = itemView.findViewById(R.id.cancel_or_book_button);
         }
     }
 }
